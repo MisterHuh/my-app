63c341613d9d45f1a35afcc3a953e2d747076cd6
MisterHuh <jaehuh86@yahoo.com>
2020-06-24 21:30:50 -0700

    practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from "redux";
+
+// STORE = globalized state
+
+// ACTION = describes what i'm doing
+
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+// REDUCER =
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+// this is to console log
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        hello
       </header>
     </div>
   );
 }
+
+
 
 export default App;
